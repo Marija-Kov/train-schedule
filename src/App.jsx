d4e6b8ffeb73b4c8ee950e8ebe7e4b69b3dd1075
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-07-10 18:57:13 +0200

    Start a React JS project with Vite, install Sass

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import './scss/main.scss'
+
+function App() {
+
+  return (
+    <>
+  <h1>Train schedule</h1>
+    </>
+  )
+}
+
+export default App
