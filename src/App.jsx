34fd31d89cfdd751868b59ed35d95f766fb2384a
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-07-13 21:40:08 +0200

    import new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-import './App.css'
 import './scss/main.scss'
+import Form from './components/Form'
+import Results from './components/Results'
 
 function App() {
+ const [showResults, setShowResults] = useState(false);
 
+ const showRes = () => {
+  setShowResults(prev => !prev)
+ }
   return (
-    <>
-  <h1>Train schedule</h1>
-    </>
+    <div className="container">
+      <h1>Train schedule</h1>
+      {!showResults ? 
+       <Form showRes={showRes}/> : 
+       <Results showRes={showRes}/>
+      } 
+    </div>
   )
 }
 
