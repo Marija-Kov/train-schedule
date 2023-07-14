e2e4523e3f92e331ab155e69de0375d27ea71d04
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-07-14 15:27:08 +0200

    implement data filtering based on week day

--- src/hooks/useGetDepartures.jsx
+++ src/hooks/useGetDepartures.jsx
@@ -3,26 +3,31 @@ import {
   trainId_direction1, 
   trainId_direction2, 
   theTimetable_direction1, 
-  theTimetable_direction2
-
+  theTimetable_direction2,
+  trainIdRidesOnWeekendsAndHolidays_direction1,
+  trainIdRidesOnWeekendsAndHolidays_direction2
 } from '../data/timetable.js'
 
 const useGetDepartures = () => {
     const getDepartures = (input) => {
      let tTable;
      let trainIdArr;
+     let weekendsAndHolidays;
      let s = stations;
      let fromIdx = s.indexOf(input.from);
      let toIdx = s.indexOf(input.to);
+     const dayOfWeek = new Date(input.date).getDay();
      if(fromIdx > toIdx){
       tTable = theTimetable_direction2;
       const revS = s.reverse();
       fromIdx = revS.indexOf(input.from);
       toIdx = revS.indexOf(input.to);
       trainIdArr = trainId_direction2;
+      weekendsAndHolidays = trainIdRidesOnWeekendsAndHolidays_direction2;
      } else {
       tTable = theTimetable_direction1;
       trainIdArr = trainId_direction1;
+      weekendsAndHolidays = trainIdRidesOnWeekendsAndHolidays_direction1;
      }
      const departures = [];
      const len = tTable[0].length;
@@ -34,7 +39,13 @@ const useGetDepartures = () => {
         departures.push({from: input.from, to: input.to, departureTime: departureTime, arrivalTime: arrivalTime, trainId: trainIdArr[i]})
       }
      }
-     return departures
+     if(dayOfWeek === 6 || dayOfWeek === 0) {
+       return departures.filter(d => {
+        const activeOnWeekendsAndHolidays = weekendsAndHolidays[trainIdArr.indexOf(d.trainId)];
+        return activeOnWeekendsAndHolidays === true || activeOnWeekendsAndHolidays === "w&h_only";
+      })
+     }
+      return departures
     }
   return {getDepartures}
 }
