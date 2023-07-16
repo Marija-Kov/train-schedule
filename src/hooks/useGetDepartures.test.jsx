fded073f6a1011370db0206d31379fecaff0d52d
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-07-16 10:49:44 +0200

    add Departure component tests

---
+++ src/hooks/useGetDepartures.test.jsx
@@ -0,0 +1,10 @@
+import {describe, it, expect} from 'vitest';
+import { renderHook } from "@testing-library/react";
+import useGetDepartures from "./useGetDepartures";
+
+describe("useGetDepartures", () => {
+ it("should return getDepartures() function", () => {
+    const { result } = renderHook(useGetDepartures);
+    expect(result.current.getDepartures).toBeTruthy(); 
+ })
+})
