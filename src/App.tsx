5c567a3aa32d63ee2204f0d4393a28c779ff8ae2
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2023-11-10 22:58:13 +0100

    Restyled no departures found message..
    - Turned it into a modal so it doesn't get lost at the bottom of the page on some mobile devices/browsers;

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,98 @@
-import { SetStateAction, useState } from 'react'
-import Form from './components/Form'
-import Departure from './components/Departure'
-import Info from './components/Info'
-import { DepartureReturned } from './types'
+import { SetStateAction, useState } from "react";
+import Form from "./components/Form";
+import Departure from "./components/Departure";
+import Info from "./components/Info";
+import { DepartureReturned } from "./types";
 
 function App() {
- const [departures, setDepartures] = useState<DepartureReturned[] | string>([]);
- const [appInfo, setAppInfo] = useState(false);
+  const [departures, setDepartures] = useState<DepartureReturned[] | string>(
+    []
+  );
+  const [appInfo, setAppInfo] = useState(false);
 
- const runSetDepartures = (d: SetStateAction<DepartureReturned[] | string>) => {
-  setDepartures(d)
- }
+  const runSetDepartures = (
+    d: SetStateAction<DepartureReturned[] | string>
+  ) => {
+    setDepartures(d);
+  };
 
- const runSetAppInfo = () => {
-  setAppInfo(prev => !prev)
- }
+  const runSetAppInfo = () => {
+    setAppInfo((prev) => !prev);
+  };
+
+  const showData = () => {
+    if (typeof departures === "object" && departures.length) {
+      return (
+        <div className="departures--container">
+          <h2>
+            <span aria-label="route start">{departures[0].from}</span>
+            <span aria-label="route end">{departures[0].to}</span>
+          </h2>
+          <h3>
+            <span>polazak</span>
+            <span>dolazak</span>
+            <span>br. voza</span>
+          </h3>
+          {departures.map((d) => (
+            <Departure
+              key={d.trainId}
+              departureTime={d.departureTime}
+              arrivalTime={d.arrivalTime}
+              trainId={d.trainId}
+            />
+          ))}
+          <button
+            aria-label="back to search form"
+            className="back"
+            onClick={() => runSetDepartures([])}
+          >
+            nazad
+          </button>
+        </div>
+      );
+    }
+    if (typeof departures === "string") {
+      return (
+        <div className="no-departures">
+          <p>⚠</p>
+          <p>Nema polazaka po tim parametrima</p>
+          <p>/</p>
+          <p>No departures found</p>
+          <button
+            aria-label="back to search form"
+            className="back"
+            onClick={() => runSetDepartures([])}
+          >
+            nazad
+          </button>
+        </div>
+      );
+    }
+    return "";
+  };
 
   return (
     <div className="container">
       <header>
         <span>&#10210;</span>
-       <a href="">
-        <img 
-        className="train-icon"
-        src="/train-icon.png"/>
-       </a> 
-       <span>
-        <button 
-        aria-label="more info"
-        onClick={runSetAppInfo}
-        className="info"
-        >
-          ?
-        </button>
-       </span>
+        <a href="">
+          <img className="train-icon" src="/train-icon.png" />
+        </a>
+        <span>
+          <button
+            aria-label="more info"
+            onClick={runSetAppInfo}
+            className="info"
+          >
+            ?
+          </button>
+        </span>
       </header>
-
-      {appInfo && <Info runSetAppInfo={runSetAppInfo}/>}     
-      <Form 
-       runSetDepartures={runSetDepartures}
-       />
-      {typeof departures === "object" && departures.length ?      
-       <div className="departures--container">
-        <h2>
-          <span aria-label="route start">
-           {departures[0].from}
-          </span>
-          <span aria-label="route end">
-           {departures[0].to}
-          </span>
-        </h2> 
-        <h3>
-          <span>polazak</span>
-          <span>dolazak</span>
-          <span>br. voza</span>
-        </h3>
-        {departures.map(d => (
-          <Departure
-           key={d.trainId}
-           departureTime={d.departureTime}
-           arrivalTime={d.arrivalTime}
-           trainId={d.trainId}
-          />
-        ))}
-         <button 
-          aria-label="back to search form"
-          className="back"
-          onClick={() => runSetDepartures([])}>
-           nazad na pretragu
-         </button> 
-        </div> : (
-          typeof departures === "string" ?
-        <div className="no-departures">
-          <p>⚠</p>
-          Nema polazaka po tim parametrima!
-        </div> : null     
-        )
-
-      }
-
+      {appInfo && <Info runSetAppInfo={runSetAppInfo} />}
+      <Form runSetDepartures={runSetDepartures} />
+      {showData()}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
