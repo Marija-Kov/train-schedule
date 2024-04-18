0779e6fcaa035f4d84952dd14a8b3765c4cf39b7
Marija-Kov <96353043+Marija-Kov@users.noreply.github.com>
2024-04-18 13:53:23 +0200

    Fix test values

--- src/components/Departure.test.tsx
+++ src/components/Departure.test.tsx
@@ -5,7 +5,7 @@ import Departure from "./Departure";
 describe("<Departure />", () => {
   it("should render Departure component properly", () => {
     render(
-      <Departure departureTime={"12.0"} arrivalTime={"12.15"} trainId={8000} />
+      <Departure departureTime={"12:0"} arrivalTime={"12:15"} trainId={8000} />
     );
     const departureTime = screen.getByLabelText("departure time");
     const arrivalTime = screen.getByLabelText("arrival time");
