3648b945227569efff92f0572fcb756137ab6e69
Marija Kovacevic <kccmrj@outlook.com>
2025-07-24 17:29:04 +0200

    silence act warnings when running tests

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom'
+import { beforeAll } from 'vitest';
+
+const originalError = console.error
+const actWarnings = [];
+
+beforeAll(() => {
+  // Suppress "Warning: An update to ComponentName inside a test was not wrapped in act(...)."
+  // because we don't need act(): 
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      actWarnings.push(args[0]);
+      return
+    }
+    originalError.call(console, ...args);
+  }
+})
