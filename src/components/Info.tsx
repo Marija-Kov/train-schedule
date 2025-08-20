9794b7248f41a052ba2cb68488b3d346757e44c1
Marija Kovacevic <kccmrj@outlook.com>
2025-08-20 10:13:38 +0200

    replace (nested) p tags with more appropriate/meaningful ones

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -5,7 +5,7 @@ const Info = () => {
   const { infoLanguage } = useContext(LanguageContext);
   return (
     <div className="info--content">
-      <p>
+      <div>
         {infoLanguage.intro}
         <a data-testid="schedule-pdf-link" href="https://www.srbvoz.rs/wp-content/redvoznje/rv_bg_voza_za_2022.pdf"> {infoLanguage.schedule_link} </a>
         {infoLanguage.valid_until}
@@ -14,11 +14,11 @@ const Info = () => {
         {infoLanguage.personal_use}
         <br></br>
         <br></br>
-        <p data-testid="note-on-schedule-changes-title">{infoLanguage.note_title}</p>
+        <h4 data-testid="note-on-schedule-changes-title">{infoLanguage.note_title}</h4>
         {infoLanguage.schedule_change_note}
         <a data-testid="schedule-change-announcements" href="https://srbijavoz.rs/informacije/"><strong> {infoLanguage.schedule_change_announcements_link} </strong></a>
         {infoLanguage.for_info}
-      </p>
+      </div>
 
       <a data-testid="repo-link" href="https://github.com/Marija-Kov/train-schedule">
         <img className="github-icon" src="/github.png" />
