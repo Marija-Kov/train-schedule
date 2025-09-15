75430e948da64fc01adbc293b60b05376f5de776
marija <kccmrj@outlook.com>
2025-09-15 09:57:27 +0200

    remove filtering by current time
    to fix missing relevant updates posted ahead of time on the given day

--- src/hooks/useTrainServiceUpdates/useTrainServiceUpdates.tsx
+++ src/hooks/useTrainServiceUpdates/useTrainServiceUpdates.tsx
@@ -7,9 +7,7 @@ const useTrainServiceUpdates = () => {
     const trainServiceUpdates = async () => {
         setLoadingUpdates(true);
         const currentDateTime = new Date().toISOString().split(".")[0];
-        const currentLocalTime = new Date().toTimeString().split(" ")[0]
         const currentDate = currentDateTime.split("T")[0];
-        const currentTimeNum = parseInt(currentLocalTime.split(":").join(""))
 
         const response = await fetch('https://www.srbvoz.rs/wp-json/wp/v2/info_post?per_page=30');
         if ([500, 404, 403, 401].includes(response.status) || !response.ok) {
@@ -24,11 +22,11 @@ const useTrainServiceUpdates = () => {
         
         const filteredUpdates = data.filter((update: { date: string; slug: string; }) => {
             const updateDate = update.date.split("T")[0];
-            const updateTimeNum = parseInt(update.date.split("T")[1].split(":").join(""));
             return updateDate === currentDate
-            && updateTimeNum >= currentTimeNum
             && update.slug.match(/bgvoz/i);
-        }).map((update: { content: { rendered: string; }; }) => update.content.rendered.split('\n')[1].slice(3, -4)
+        }).map((update: { content: { rendered: string; }; }) => {
+            return update.content.rendered.split('\n')[1].slice(3, -4)
+        }
         ).filter((update: string | string[]) => {
             return update.indexOf("Resnik") === -1 && update.indexOf("Mladenov") === -1 && update.indexOf("Lazarev") === -1
         });
